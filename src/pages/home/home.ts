9a1cf77ba16d2a2e525401248fdbaf2b13fcecce
JANDERSON-PC\JANDERSON <jssystems2021@gmail.com>
2021-02-03 13:21:28 -0300

    infiniteScroll

---
+++ src/pages/home/home.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+@Component({
+  selector: 'infinite-scroll-example',
+  templateUrl: 'infinite-scroll-example.html',
+  styleUrls: ['./infinite-scroll-example.css']
+})
+export class HomePage {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
+  constructor() {}
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (data.length == 1000) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  }
+
+ 
+}
